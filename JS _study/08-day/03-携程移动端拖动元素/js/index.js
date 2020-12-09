d03bf4af3d2985e833aa3d98e3ede82d895be259
g130133875 <gzhhhxx@icloud.com>
2020-12-09 14:30:47 +0800

    携程滑动

--- JS _study/08-day/03-携程移动端拖动元素/js/index.js
+++ JS _study/08-day/03-携程移动端拖动元素/js/index.js
@@ -1,11 +1,73 @@
 window.addEventListener('load',function(){
     var focus = document.querySelector('.focus');
     var focusWidth = focus.offsetWidth;
-    var lis = focus.querySelector('ol').querySelectorAll('li');
-    for(var i=0;i<lis.length;i++){
-        lis[i].setAttribute('index',i);
-        lis[i].addEventListener('click',function(){
-            console.log(this.getAttribute('index'));
-        })
-    }
+    var ul = focus.children[0];
+    var ol = focus.children[1];
+    var w = focus.offsetWidth;
+    var index = 0;
+    var timer = setInterval(function(){
+        index++;
+        var translatex = -index * w;
+        ul.style.transition = 'all .3s';
+        ul.style.transform = `translateX(${translatex}px)`;
+    },2000);
+    ul.addEventListener('transitionend',function(){
+        if(index >= 3){
+            index = 0;
+            ul.style.transition = 'none';
+            var translatex = -index * w;
+            ul.style.transform = `translateX(${translatex}px)`;
+        }else if(index<0){
+            index = 2;
+            ul.style.transition = 'none';
+            var translatex = -index * w;
+            ul.style.transform = `translateX(${translatex}px)`;
+        }
+        ol.querySelector('.current').classList.remove('current');
+        ol.children[index].classList.add('current');
+    });
+
+    //手指滑动轮播图
+    var startX = 0;
+    var moveX = 0;
+    var flag = false;
+    ul.addEventListener('touchstart',function(e){
+        startX = e.targetTouches[0].pageX;
+        clearInterval(timer);
+    });
+    ul.addEventListener('touchmove',function(e){
+        moveX = e.targetTouches[0].pageX - startX;
+        var translatex = -index*w + moveX;
+        ul.style.transition = 'none';
+        ul.style.transform = `translateX(${translatex}px)`;
+        flag = true;  //如果滑动在判断
+        e.preventDefault(); 
+    })
+    ul.addEventListener('touchend',function(e){
+        if(flag){
+            if(Math.abs(moveX)>50){
+                if(moveX>0){
+                    index--;
+                }
+                else{
+                    index++;
+                }
+                var translatex = -index * w;
+                ul.style.transition = 'all .3s';
+                ul.style.transform = `translateX(${translatex}px)`;
+            }else{
+                var translatex = -index*w;
+                ul.style.transition = 'all .1s';
+                ul.style.transform = `translateX(${translatex}px)`;
+            }
+        }
+        clearInterval(timer);
+        timer = setInterval(function(){
+            index++;
+            var translatex = -index * w;
+            ul.style.transition = 'all .3s';
+            ul.style.transform = `translateX(${translatex}px)`;
+        },2000);
+    })
+
 })
