b7064301af1bc4e17896c4ddec0fd77b72139a1e
g130133875 <gzhhhxx@icloud.com>
2021-01-12 16:29:46 +0800

    调整

--- vue_study/demo/tabbar/src/components/tabbar/TabBarItem.vue
+++ vue_study/demo/tabbar/src/components/tabbar/TabBarItem.vue
@@ -2,18 +2,29 @@
     <div class="tab-bar-item" @click="choose">
       <div v-if="isActive"><slot name="item-icon-active"></slot></div>
       <div v-else><slot name="item-icon"></slot></div>
-      <div :class="{active: isActive}"><slot name="item-text"></slot></div>
+      <div :style="activeStyle"><slot name="item-text"></slot></div>
     </div>
 </template>
 <script>
 export default {
   name: 'TabBarItem',
   props: {
-    path: String
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red',
+    }
   },
   data() {
     return {
-      isActive: false,
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) != -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
     }
   },
   methods: {
@@ -36,8 +47,5 @@ export default {
     vertical-align: middle;
     margin-top: 5px;
     margin-bottom: 2px;
-  }
-  .active {
-    color: red;
   }
 </style>
--- vue_study/webpack/src/info.js
+++
@@ -1,3 +0,0 @@
-export const name = 'Gerard Schinner'
-export const gender = 'male'
-export const height = 1.88
