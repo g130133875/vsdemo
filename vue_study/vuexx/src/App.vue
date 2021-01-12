195626249180a8381a64a5e09ffc27ba8782da19
g130133875 <gzhhhxx@icloud.com>
2021-01-12 23:16:24 +0800

    getters

--- vue_study/vuexx/src/App.vue
+++ vue_study/vuexx/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <h2>{{$store.state.counter}}</h2>
+    <!-- <h2>{{$store.state.counter}}</h2>
     <button @click="increase">+</button>
-    <button @click="decrease">-</button>
+    <button @click="decrease">-</button> -->
+    <h2>{{$store.state.students}}</h2>
+    <h2>{{$store.getters.morethan18}}</h2>
+    <h2>{{$store.getters.morethan18Len}}</h2>
+    <h2>{{$store.getters.morethan(18)}}</h2>
     <h2>----------------------------------</h2>
     <test-vuex/>
   </div>
--- vue_study/vuexx/src/store/index.js
+++ vue_study/vuexx/src/store/index.js
@@ -8,10 +8,15 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     //共享的数据
-    counter: 100
+    counter: 100,
+    students: [
+      {id: 100, name: 'aa', age: 18},
+      {id: 101, name: 'aaa', age: 19},
+      {id: 102, name: 'aaaa', age: 20},
+    ]
   },
   mutations: {
-    //方法
+    //mutation方法，只能是同步操作
     increment(state) {
       state.counter++
     },
@@ -23,7 +28,21 @@ const store = new Vuex.Store({
 
   },
   getters: {
-
+    // 防止getters方法
+    morethan18(state) {
+      return state.students.filter(s => s.age >18)
+    },
+    morethan18Len(state, getters) {
+      return getters.morethan18.length
+    },
+    morethan(state) {
+      // return function(age) {
+      //   return state.students.filter(s => s.age > age)
+      // }
+      return age => {
+        return state.students.filter(s => s.age > age)
+      }
+    }
   },
   modules: {
 
