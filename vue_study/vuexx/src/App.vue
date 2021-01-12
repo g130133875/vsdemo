b450bb243a3490e6ab78fe51fe42415b706efcc1
g130133875 <gzhhhxx@icloud.com>
2021-01-12 22:21:47 +0800

    1.12

---
+++ vue_study/vuexx/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+    <h2>----------------------------------</h2>
+    <test-vuex/>
+  </div>
+</template>
+
+<script>
+import TestVuex from '@/components/TestVuex.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestVuex
+  },
+  methods: {
+    increase() {
+      this.$store.commit('increment')
+    },
+    decrease() {
+      this.$store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ vue_study/vuexx/src/components/TestVuex.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>{{$store.state.counter}}</h2>
+</template>
+<script>
+export default {
+  name: 'TestVuex'
+}
+</script>
