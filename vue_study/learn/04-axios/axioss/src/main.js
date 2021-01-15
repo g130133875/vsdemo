1068569394dea6f0a7aca8947f576c2c551594a6
g130133875 <gzhhhxx@icloud.com>
2021-01-15 15:18:23 +0800

    网络请求

--- vue_study/learn/04-axios/axioss/src/main.js
+++ vue_study/learn/04-axios/axioss/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
+// import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -26,13 +26,35 @@ new Vue({
 //   console.log(res);
 // })
 
-// 2.axios发送并发请求
-axios.defaults.baseURL = 'http://123.207.32.32:8000'
-axios.defaults.timeout = 5000
-axios.all([axios({
-  url: '/home/multidata',
-}),axios({
-  url: '/home/multidata',
-})]).then(axios.spread((res1, res2) => {
-  console.log(res1,res2);
-}))
+// // 2.axios发送并发请求
+// axios.defaults.baseURL = 'http://123.207.32.32:8000'
+// axios.defaults.timeout = 5000
+// axios.all([axios({
+//   url: '/home/multidata',
+// }),axios({
+//   url: '/home/multidata',
+// })]).then(axios.spread((res1, res2) => {
+//   console.log(res1,res2);
+// }))
+
+
+// 3. 创建对应的axios实例
+// const instance1 = axios.create({
+//   baseURL: 'http://123.207.32.32:8000',
+//   timeout: 5000
+// })
+
+// instance1({
+//   url: '/home/multidata'
+// }).then(res => {
+//   console.log(res);
+// })
+
+import {request} from './network/request'
+request({
+  url: 'home/multidata',
+}).then(res => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+})
