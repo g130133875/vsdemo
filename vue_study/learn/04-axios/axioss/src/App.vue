b010f4069e4458968a04887e35327dd1987140b9
g130133875 <gzhhhxx@icloud.com>
2021-03-22 22:10:55 +0800

    组件通信

--- vue_study/learn/04-axios/axioss/src/App.vue
+++ vue_study/learn/04-axios/axioss/src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="app">
-
-  </div>
+  <hello-world message="aaa"/>
 </template>
 
 <script>
-
-
+import HelloWorld from '../../../组件通信/communicate/src/components/HelloWorld.vue'
+import HelloWorld from ('./components/HelloWorld')
 export default {
+  components: { HelloWorld },
   name: 'App',
 }
 </script>
